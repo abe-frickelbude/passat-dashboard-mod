46f196518b6ccaee4f024c765e311a9504905252
abe <ibragim.kuliev@googlemail.com>
2018-01-09 23:59:26 +0100

    WIP port of old Kalman filter processing sketch to processing 3.x
    - Maven build is busted right now because the mvn repo "processing-core"
    and "processing-serial" artifacts are messed up, will need to rely on
    local processing 3.x installation instead <sigh...>

---
+++ adc-monitor/src/main/java/de/fb/processing/arduino/KalmanFilter.java
@@ -0,0 +1,47 @@
+package de.fb.processing.arduino;
+
+public class KalmanFilter {
+
+    private final float q;
+    private final float r;
+    private float x;
+    private float p;
+    private float k;
+
+    public KalmanFilter(final float q, final float r, final float p, final float initial_value) {
+        this.q = q;
+        this.r = r;
+        this.p = p;
+        x = x;
+    }
+
+    public float addSample(final float measurement) {
+        // omit x=x
+        p = p + q;
+        k = p / (p + r);
+        x = x + k * (measurement - x);
+        p = (1 - k) * p;
+        return x;
+    }
+
+    @Override
+    public String toString() {
+        return "KalmanFilter with p=" + p + ", k=" + k;
+    }
+
+    public float getQ() {
+        return q;
+    }
+
+    public float getR() {
+        return r;
+    }
+
+    public float getP() {
+        return p;
+    }
+
+    public float getK() {
+        return k;
+    }
+}
