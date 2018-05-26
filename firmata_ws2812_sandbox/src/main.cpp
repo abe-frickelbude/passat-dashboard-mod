879e1ff86410a1fbbf23c5216a3b5329613bc879
abe <ibragim.kuliev@googlemail.com>
2018-05-27 00:53:55 +0200

    WIP command parser

--- firmata_ws2812_sandbox/src/main.cpp
+++ firmata_ws2812_sandbox/src/main.cpp
@@ -1,32 +1,49 @@
-/*
- * Firmata is a generic protocol for communicating with microcontrollers
- * from software on a host computer. It is intended to work with
- * any host computer software package.
- *
- * To download a host software package, please click on the following link
- * to open the download page in your default browser.
- *
- * http://firmata.org/wiki/Download
- */
 
-/* This sketch accepts strings and raw sysex messages and echos them back.
- *
- * This example code is in the public domain.
- */
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <Firmata.h>
 
-void stringCallback(char *string);
+#include "fab_led.h"
+
+// custom SysEx command for Firmata
+const byte PIXEL_COMMAND = 0x40;
+
+// --------------------- LED state ---------------------------
+
+const uint8_t NUM_PIXELS = 3;
+
+// Min / max LED brightness (max possible 255)
+const uint8_t MIN_BRIGHTNESS = 0;
+const uint8_t MAX_BRIGHTNESS = 20;
+
+// pixel state array
+rgbw pixels[NUM_PIXELS] = {};
+
+volatile byte currentBrightness = 1;
+volatile bool stateChanged = false;
+
+volatile bool testLedOn = false;
+
+// SK6812 strip on PORT D, pin 6
+sk6812b<D, 6> strip;
+
+// ------------------------------------------------------------
+
 void sysexCallback(byte command, byte argc, byte *argv);
 
 void setup()
 {
-    Firmata.setFirmwareNameAndVersion("Firmata Echo", FIRMATA_FIRMWARE_MAJOR_VERSION, FIRMATA_FIRMWARE_MINOR_VERSION);
-    Firmata.attach(STRING_DATA, stringCallback);
+    Firmata.setFirmwareNameAndVersion("WS2812 Sandbox", FIRMATA_FIRMWARE_MAJOR_VERSION, FIRMATA_FIRMWARE_MINOR_VERSION);
     Firmata.attach(START_SYSEX, sysexCallback);
-    Firmata.begin(57600);
+    Firmata.begin(57600); // ?! Can't use a previously defined uint16_t constant here!
+
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
@@ -37,12 +54,16 @@ void loop()
     }
 }
 
-void stringCallback(char *myString)
-{
-    Firmata.sendString(myString);
-}
-
 void sysexCallback(byte command, byte argc, byte *argv)
 {
-    Firmata.sendSysex(command, argc, argv);
+    switch (command)
+    {
+    case PIXEL_COMMAND:
+        testLedOn = !testLedOn;
+        digitalWrite(LED_BUILTIN, testLedOn ? HIGH : LOW);
+        break;
+
+    default:
+        break;
+    }
 }
